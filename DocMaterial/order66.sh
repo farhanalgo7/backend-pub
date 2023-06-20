0d5b2e173e521b369e40807c0bb8796b66bdab12
farhanalgo7 <126077411+farhanalgo7@users.noreply.github.com>
2023-06-20 16:17:50 +0530

    Update order66.sh

--- DocMaterial/order66.sh
+++ DocMaterial/order66.sh
@@ -3,7 +3,7 @@ cd docs/
 printf 'n\n\nDocForFabric\n\nABC\n\n1\n\n' | sphinx-quickstart
 cd ..
 cp ./DocMaterial/index.rst ./DocMaterial/conf.py ./docs
-sphinx-apidoc -o docs ./src_for_doc./backend-pub
+sphinx-apidoc -o docs ./src_for_doc./backend_pub
 tree
 cat docs/conf.py
 # rm docs/conf.rst docs/crawler.rst
